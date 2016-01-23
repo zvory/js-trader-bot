a69610830337be335b43f2fcc7e90c9934d33b61
Matthew Chung <mattpkqwe@gmail.com>
2016-01-23 15:45:19 -0500

    marketlogic

---
+++ marketLogic.js
@@ -0,0 +1,24 @@
+var MarketLogic = function() {
+
+}
+
+
+var Books = function() {
+	this.books ={
+		BOND:false,
+		VALBZ:false, 
+		VALE: false,
+		GS: false,
+		MS: false,
+		WFC: false,
+		XLF: false
+	};
+};
+
+Books.prototype.updateBook = function(book) {
+	this.books[book.symbol] = book;
+};
+
+Books.prototype.getBook = function(type) {
+	return this.books[type];
+};
