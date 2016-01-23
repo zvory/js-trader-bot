ec0e1f1a58a314b65c6bb25e597ea0d090117f27
Alexander Zvorygin <livingalex@gmail.com>
2016-01-23 11:26:29 -0500

    basic bot

---
+++ bot.js
@@ -0,0 +1,25 @@
+var net = require('net');
+
+var HOST = '';
+var PORT = 9090;
+// Create a server instance, and chain the listen function to it
+// The function passed to net.createServer() becomes the event handler for the 'connection' event
+// The sock object the callback function receives UNIQUE for each connection
+net.createServer(function(sock) {
+    
+    // We have a connection - a socket object is assigned to the connection automatically
+    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
+    
+    // Add a 'data' event handler to this instance of socket
+    sock.on('data', function(data) {
+    });
+
+    // Add a 'close' event handler to this instance of socket
+    sock.on('close', function(data) {
+        console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
+    });
+    
+}).listen(PORT, HOST);
+
+
+console.log('bot listening to' + HOST +':'+ PORT);
